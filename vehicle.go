a2f5d143cbc5dcadca3b417f943a2ac10de620a7
vi.shenoy <vi.shenoy@f5.com>
2022-05-06 01:08:26 +0800

    v0.2

--- vehicle.go
+++ vehicle.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
 type vehicle interface {
 }
 
@@ -74,11 +82,11 @@ func init() {
 func main() {
 
 	// Generate ratings for the different vehicles
+	generateRating()
 
 	// Print ratings for the different vehicles
 }
 
-/*
 func readJSONFile() Values {
 	jsonFile, err := os.Open("feedback.json")
 
@@ -94,4 +102,40 @@ func readJSONFile() Values {
 
 	return content
 }
-*/
+
+func generateRating() {
+	var vehResult feedbackResult
+	var vehRating rating
+	f := readJSONFile()
+	for _, v := range f.Models {
+		for msg := range v.feedback {
+			if text := strings.Split(msg, " "); len(text) >= 5 {
+				vehRating = 5.0
+				vehResult.feedbackTotal++
+				for word := range text {
+					s := strings.Trim(strings.ToLower(word), " ,.,!,?,\t,\n,\r")
+					switch s {
+					case "pleasure", "impressed", "wonderful", "fantastic", "splendid":
+						vehRating += extraPositive
+					case "help", "helpful", "thanks", "thank you", "happy":
+						vehRating += positive
+					case "not helpful", "sad", "angry", "improve", "annoy":
+						vehRating += negative
+					case "pathetic", "bad", "worse", "unfortunately", "agitated", "frustrated":
+						vehRating += extraNegative
+					}
+				}
+				switch true {
+				case vehRating > 8.0:
+					vehResult.feedbackPositive++
+				case vehRating >= 4.0 && vehRating <= 8.0:
+					vehResult.feedbackNeutral++
+				case vehRating < 4.0:
+					vehResult.feedbackNegative++
+				}
+			}
+		}
+		vehicleResult[v.Name] = vehResult
+
+	}
+}
